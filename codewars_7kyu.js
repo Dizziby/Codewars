4b1291b22f30330b017dd037da49cd2943ffb23d
Dizziby <Dizzi_by@mail.ru>
2022-02-28 23:14:32 +0300

    Task 7kyu 1-6

--- codewars_7kyu.js
+++ codewars_7kyu.js
@@ -24,3 +24,51 @@ function reverseWords(str) {
 
 
 // -----3-----
+function highAndLow(numbers){
+    const arr = numbers.split(" ");
+    let max = Math.max.apply(null, arr);
+    let min = Math.min.apply(null, arr);
+    return `${max} ${min}`;
+}
+
+
+// -----4-----
+function matrix(array) {
+    array.forEach((elem, index) => {
+        for (let i = 0; i < elem.length; i++) {
+            if(index === i && elem[i] < 0) {
+                elem[i] = 0;
+            } else if (index === i) {
+                elem[i] = 1;
+            }
+        }
+    });
+    return array;
+}
+
+
+// -----5-----
+function vaporcode(string) {
+    const arr = string.toUpperCase().split(" ").join("").split("");
+    console.log(arr);
+    let newString = arr.join("  ");
+    return newString;
+}
+
+
+// -----6-----
+function vowelIndices(word){
+    let regExp = /[aeiouy]/i;
+    const arr = word.split("");
+    const results = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(regExp.test(arr[i])) {
+            results.push(i+1);
+        }
+    }
+    return results;
+}
+
+
+// -----7-----
+
