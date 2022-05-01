2885ca4b25170e2450026cb5f7f34045e1c06be0
Dizziby <Dizzi_by@mail.ru>
2022-05-01 15:14:41 +0300

    Task 8kyu 1-196

--- codewars_8kyu.js
+++ codewars_8kyu.js
@@ -1970,3 +1970,9 @@ const cannonsReady = (gunners) => {
     }
     return 'Fire!'
 }
+
+
+// -----196-----
+function correct(string) {
+    return string.replace(/5/g, "S").replace(/0/g, "O").replace(/1/g, "I")
+}
