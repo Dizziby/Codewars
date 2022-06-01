47dc8dc7ef61180744eeb52fc9df097509c5b678
Dizziby <Dizzi_by@mail.ru>
2022-06-01 23:23:14 +0300

    Task 8kyu 1-207

--- codewars_8kyu.js
+++ codewars_8kyu.js
@@ -2082,3 +2082,15 @@ function billboard(name, price = 30){
     }
     return str.length
 }
+
+
+// -----207-----
+function howManyDalmatians(number) {
+
+    var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+
+    var respond = number <= 10 ? dogs[0] : (number <= 50 ? dogs[1] : (number === 101 ? dogs[3] : dogs[2]))
+
+    return respond
+}
+
