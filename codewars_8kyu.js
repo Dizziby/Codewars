a0853b16c341dfe1a538d255b80f7e8fe6ab3ea1
Dizziby <Dizzi_by@mail.ru>
2022-07-12 22:21:00 +0300

    Task 8kyu 1-216

--- codewars_8kyu.js
+++ codewars_8kyu.js
@@ -2181,3 +2181,18 @@ class Cube {
         this.side = Math.abs(n);
     }
 }
+
+
+// -----216-----
+function finalGrade (exam, projects) {
+    if(exam > 90 || projects > 10) {
+        return 100
+    }
+    if(exam > 75 && projects >= 5) {
+        return 90
+    }
+    if(exam > 50 && projects >= 2) {
+        return 75
+    }
+    return 0
+}
