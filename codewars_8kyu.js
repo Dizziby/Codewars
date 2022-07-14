a51a9587ab3cebe241726966848c7b75d101b8ca
Dizziby <Dizzi_by@mail.ru>
2022-07-14 23:24:18 +0300

    Task 8kyu 1-218

--- codewars_8kyu.js
+++ codewars_8kyu.js
@@ -2205,3 +2205,24 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
     }
     return false
 };
+
+
+// -----218-----
+function sumArray(array) {
+    if(array === null) {
+        return 0
+    }
+    if(array === undefined) {
+        return 0
+    }
+    if(array.length === 0) {
+        return 0
+    }
+    if(array.length === 1) {
+        return 0
+    }
+    if(array.length === 2) {
+        return 0
+    }
+    return array.reduce((acc, el) => acc + el, 0) - Math.max(...array) - Math.min(...array)
+}
