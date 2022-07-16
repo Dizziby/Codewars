7a020e51de5dc85f9720d1d5ae8735900f4f5075
Dizziby <Dizzi_by@mail.ru>
2022-07-16 23:28:43 +0300

    Task 8kyu 1-221

--- codewars_8kyu.js
+++ codewars_8kyu.js
@@ -2233,3 +2233,59 @@ function helloWorld() {
     let str = "Hello World!"
     console.log(str)
 }
+
+
+// -----220-----
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+    //select some variable to combine "Dad"
+    return d1+a2+d2;
+}
+function Bee(){
+    //select some variable to combine "Bee"
+    return b1+e2+e2;
+}
+function banana(){
+    //select some variable to combine "banana"
+    return b2+a2+n2+a2+n2+a2;
+}
+
+//answer some questions if you finished works above
+function answer1(){
+    //the answer should be "yes" or "no"
+    return "no";
+}
+function answer2(){
+    //the answer should be "yes" or "no"
+    return "no";
+}
+function answer3(){
+    //the answer should be "yes" or "no"
+    return "yes";
+}
+
+
+// -----221-----
+function getLength(arr){
+    //return length of arr
+    return arr.length
+}
+function getFirst(arr){
+    //return the first element of arr
+    return arr[0]
+}
+function getLast(arr){
+    //return the last element of arr
+    return arr[arr.length - 1]
+}
+function pushElement(arr){
+    var el=1;
+    arr.push(el)
+
+    return arr
+}
+function popElement(arr){
+    arr.pop()
+
+    return arr
+}
