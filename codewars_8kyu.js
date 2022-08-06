a484a9f7461cf8b6b609f3ba444191c2582ecfba
Dizziby <Dizzi_by@mail.ru>
2022-08-06 08:01:27 +0300

    Task 8kyu 1-229

--- codewars_8kyu.js
+++ codewars_8kyu.js
@@ -2373,3 +2373,22 @@ function mouthSize(animal) {
     }
     return "wide"
 }
+
+
+// -----229-----
+function howManydays(month){
+    switch (month){
+        case 4:
+            return 30;
+        case 6:
+            return 30
+        case 9:
+            return 30
+        case 11:
+            return 30
+        case 2 :
+            return 28
+        default:
+            return 31
+    }
+}
