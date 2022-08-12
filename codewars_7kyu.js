b0e24d56764bdd2c11b9e9a8bdb17e0031dea832
Dizziby <Dizzi_by@mail.ru>
2022-08-12 21:37:10 +0300

    Task 7kyu 1-115

--- codewars_7kyu.js
+++ codewars_7kyu.js
@@ -1277,3 +1277,22 @@ const last = xs => {
 function addBinary(a,b) {
     return (a + b).toString(2)
 }
+
+
+// -----115-----
+function fizzbuzz(n)
+{
+    const arr = []
+    for(let i = 1; i <= n; i++) {
+        if (i % 5 == 0 && i % 3 === 0) {
+            arr.push("FizzBuzz")
+        } else if(i % 3 === 0) {
+            arr.push("Fizz")
+        } else if(i % 5 === 0) {
+            arr.push("Buzz")
+        } else {
+            arr.push(i)
+        }
+    }
+    return arr
+}
