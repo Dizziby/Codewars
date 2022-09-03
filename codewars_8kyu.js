aba067297f52dac73313e7c1dff9d84dc3d200e7
Dizziby <Dizzi_by@mail.ru>
2022-09-03 08:04:52 +0300

    Task 8kyu 1-237

--- codewars_8kyu.js
+++ codewars_8kyu.js
@@ -2470,3 +2470,14 @@ const flip=(d, a)=>{
         return a.sort((a,b) => a - b)
     }
 }
+
+
+// -----237-----
+function firstNonConsecutive (arr) {
+    for(let i = 1; i < arr.length; i++) {
+        if(arr[i] - arr[i-1] !== 1) {
+            return arr[i]
+        }
+    }
+    return null
+}
