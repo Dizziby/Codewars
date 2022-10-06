2d4949626bd516a0d121a879741c2ac26d1c5751
Pavel Kukayeu <dizzi_by@mail.ru>
2022-10-06 22:49:32 +0300

    Task 8kyu 1-246

--- codewars_8kyu.js
+++ codewars_8kyu.js
@@ -2557,3 +2557,17 @@ function trueOrFalse(val){
     if (val)    return "true";
     else     return "false";
 }
+
+
+// -----246-----
+function pickIt(arr){
+    var odd=[],even=[];
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] % 2 !== 0) {
+            odd.push(arr[i])
+        } else {
+            even.push(arr[i])
+        }
+    }
+    return [odd,even];
+}
